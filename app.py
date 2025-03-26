36aebd6cef96b96bea50f2dff9d3abf3958cd5dd
Patrick Ridley <pari8094@colorado.edu>
2025-03-26 02:43:21 +0000

    updated app.py and adjusted requirements for db

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from Patrick Ridley in 3308!'
+    return 'Hello, World from Patrick Ridley in 3308'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.conn("postgresql://flask_hello_world_lab10_db_user:kD8xdq4ABY0dsj2mZPqvhY3jCSolD8HH@dpg-cvhmiaqqgecs73d28a00-a/flask_hello_world_lab10_db")
+    conn.close()
+    return 'Database connection successful - pari8094'

adc403d62bc213c62bc6af0b9e9c9dba1ca3ad52
Patrick Ridley <pari8094@colorado.edu>
2025-03-26 02:46:40 +0000

    fixed typo in conn

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-    conn = psycopg2.conn("postgresql://flask_hello_world_lab10_db_user:kD8xdq4ABY0dsj2mZPqvhY3jCSolD8HH@dpg-cvhmiaqqgecs73d28a00-a/flask_hello_world_lab10_db")
+    conn = psycopg2.connect("postgresql://flask_hello_world_lab10_db_user:kD8xdq4ABY0dsj2mZPqvhY3jCSolD8HH@dpg-cvhmiaqqgecs73d28a00-a/flask_hello_world_lab10_db")
     conn.close()
     return 'Database connection successful - pari8094'
